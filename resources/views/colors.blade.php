20328971752e305108b11c4ff27bba4b28efab08
Koramit Pichanaharee <koramit.p@gmail.com>
2022-05-23 15:34:22 +0700

    style code

--- resources/views/colors.blade.php
+++ resources/views/colors.blade.php
@@ -28,20 +28,20 @@
             </div>
         </div>
         <div class="w-1/2">
-            <div class="h-32 bg-soft-theme-light">
-                bg-soft-theme-light
+            <div class="h-32 bg-primary">
+                bg-primary
             </div>
-            <div class="h-32 bg-alt-theme-light">
-                bg-alt-theme-light
+            <div class="h-32 bg-primary-darker">
+                bg-primary-darker
             </div>
-            <div class="h-32 bg-bitter-theme-light">
-                bg-bitter-theme-light
+            <div class="h-32 bg-accent">
+                bg-accent
             </div>
-            <div class="h-32 bg-thick-theme-light">
-                bg-thick-theme-light
+            <div class="h-32 bg-complement">
+                bg-complement
             </div>
-            <div class="h-32 bg-dark-theme-light">
-                bg-dark-theme-light
+            <div class="h-32 bg-complement-darker">
+                bg-complement-darker
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'soft-theme-light': '#FDF6E3',
-        'alt-theme-light': '#EEE9D5',
-        'bitter-theme-light': '#AD9C68',
-        'thick-theme-light': '#586E75',
-        'dark-theme-light': '#465C62',
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'soft-theme-light': '#FDF6E3',
+                'alt-theme-light': '#EEE9D5',
+                'bitter-theme-light': '#AD9C68',
+                'thick-theme-light': '#586E75',
+                'dark-theme-light': '#465C62',
 
-        'primary': '#FDF6E3', // soft-theme-light
-        'primary-darker': '#EEE9D5', // alt-theme-light
-        'accent': '#AD9C68', // bitter-theme-light
-        'complement': '#586E75', // thick-theme-light
-        'complement-darker': '#465C62', // dark-theme-light
-      }
+                'primary': '#FDF6E3', // soft-theme-light
+                'primary-darker': '#EEE9D5', // alt-theme-light
+                'accent': '#AD9C68', // bitter-theme-light
+                'complement': '#586E75', // thick-theme-light
+                'complement-darker': '#465C62', // dark-theme-light
+            }
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
