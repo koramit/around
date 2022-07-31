987e38dc675c063e7190fe48825e9e1c721112f4
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-01 01:27:23 +0700

    add menu

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -10,6 +10,14 @@ class PreferenceController extends Controller
 {
     public function show()
     {
+        session()->flash('page-title', __('Preferences'));
+        session()->flash('main-menu-links', collect([
+            ['icon' => 'patient', 'label' => 'Patients', 'route' => route('patients'), 'can' => $request->user()->can('view_any_patients')],
+            ['icon' => 'clinic', 'label' => 'Clinics', 'route' => route('clinics'), 'can' => $request->user()->can('view_any_patients')],
+            ['icon' => 'procedure', 'label' => 'Procedures', 'route' => route('procedures.index'), 'can' => $request->user()->can('view_any_patients')],
+        ])->filter(fn ($link) => $link['can'])->values());
+        session()->flash('action-menu', []);
+
         return Inertia::render('User/PreferencePage');
     }
 
