65a5decafcedd0814d8f618d13268a9fb14c1961
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-01 16:37:17 +0700

    build

---
+++ public/build/assets/User.261a88ee.js
@@ -0,0 +1,1 @@
+import{u as t,_ as a}from"./useManageLocale.06bed602.js";import{o,c as r,b as n,t as c,d as l,F as p}from"./app.afd383f4.js";const u={class:"text-3xl font-semibold underline text-blue-700"},f={__name:"User",props:{trans:{type:Object,default:null}},setup(e){return t(e.trans),(s,m)=>(o(),r(p,null,[n("h1",u,c(s.__("user")),1),l(a)],64))}};export{f as default};
--- public/build/assets/throttle.a599e3f0.js
+++
@@ -1,1 +0,0 @@
-import{d as n}from"./debounce.ef29f51c.js";import{y as l}from"./app.5039c909.js";var d=n,g=l,c="Expected a function";function f(i,a,r){var e=!0,t=!0;if(typeof i!="function")throw new TypeError(c);return g(r)&&(e="leading"in r?!!r.leading:e,t="trailing"in r?!!r.trailing:t),d(i,a,{leading:e,maxWait:a,trailing:t})}var m=f;export{m as t};
