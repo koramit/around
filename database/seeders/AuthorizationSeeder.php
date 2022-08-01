b608676623b7d39ea03c0178a802f6e3f14046ba
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-01 16:04:15 +0700

    refactor seeder

--- database/seeders/AuthorizationSeeder.php
+++ database/seeders/AuthorizationSeeder.php
@@ -9,16 +9,9 @@ class AuthorizationSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create(['login' => 'nurse1.ahd'])->roles()->attach([3, 4]); // participant, nurse;
-        User::factory()->create(['login' => 'nurse2.ahd'])->roles()->attach([3, 4]); // participant, nurse;
-        User::factory()->create(['login' => 'manager1.ahd'])->roles()->attach([2, 3, 4, 5]); // authority, participant, nurse, manager;
-        User::factory()->create(['login' => 'manager2.ahd'])->roles()->attach([2, 3, 4, 5]); // authority, participant, nurse, manager;
-        User::factory()->create(['login' => 'fellow1.ahd'])->roles()->attach([3, 6]); // participant, fellow;
-        User::factory()->create(['login' => 'fellow2.ahd'])->roles()->attach([3, 6]); // participant, fellow;
-        User::factory()->create(['login' => 'fellow3.ahd'])->roles()->attach([3, 6]); // participant, fellow;
-        User::factory()->create(['login' => 'fellow4.ahd'])->roles()->attach([3, 6]); // participant, fellow;
-        User::factory()->create(['login' => 'staff1.ahd'])->roles()->attach([2, 3, 6, 7]); // authority, participant, fellow, staff;
-        User::factory()->create(['login' => 'staff2.ahd'])->roles()->attach([2, 3, 6, 7]); // authority, participant, fellow, staff;
+        User::factory()->create(['login' => 'root.app'])->roles()->attach([1]); // authority, participant, nurse, manager;
+        User::factory()->create(['login' => 'manager.ahd'])->roles()->attach([2, 3, 4, 5]); // authority, participant, nurse, manager;
+        User::factory()->create(['login' => 'staff.ahd'])->roles()->attach([2, 3, 6, 7]); // authority, participant, nurse, manager;
         User::factory()
             ->count(20)
             ->create();
