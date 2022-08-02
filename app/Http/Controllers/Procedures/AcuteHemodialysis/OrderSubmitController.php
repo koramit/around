cd931d54ce5a96dfda05ec26bdd8f06f245d3241
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-02 10:34:13 +0700

    redirect to previous after submit

--- app/Http/Controllers/Procedures/AcuteHemodialysis/OrderSubmitController.php
+++ app/Http/Controllers/Procedures/AcuteHemodialysis/OrderSubmitController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Procedures\AcuteHemodialysis;
 
 use App\Actions\Procedures\AcuteHemodialysis\OrderSubmitAction;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 
 class OrderSubmitController extends Controller
@@ -13,6 +14,13 @@ class OrderSubmitController extends Controller
         $data = (new OrderSubmitAction)(data: $request->all(), hashedKey: $hashedKey, user: $request->user());
 
         // if request want json return $data
+
+        if ($previous = $request->session()->pull('acute-hd-order-edit-previous-route')) {
+            try {
+                return redirect()->route($previous)->with('message', $data['message']);
+            } catch (Exception) {
+            }
+        }
 
         return redirect()->route('procedures.acute-hemodialysis.edit', $data['note']->caseRecord->hashed_key)->with('message', $data['message']);
     }
