f48f867805176d61a93ef89c7b6588b422fb1941
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-02 18:13:10 +0700

    build

---
+++ public/build/assets/PreferencePage.4075ac9b.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./app.9b65fd31.js";const r={};function c(n,_){return" Preferences "}var t=e(r,[["render",c]]);export{t as default};
--- public/build/assets/PreferencePage.c94683f6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./app.ef532a02.js";const r={};function c(n,_){return" Preferences "}var t=e(r,[["render",c]]);export{t as default};
