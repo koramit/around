1df10dc9510580ced8902eaf1e62443223257965
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-04 23:18:39 +0700

    build

---
+++ public/build/assets/PreferencePage.1341024b.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./app.e66f0e83.js";const r={};function c(n,_){return" Preferences "}var t=e(r,[["render",c]]);export{t as default};
--- public/build/assets/PreferencePage.4c805082.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./app.015842c7.js";const r={};function c(n,_){return" Preferences "}var t=e(r,[["render",c]]);export{t as default};
