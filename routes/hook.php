eedf4cc016893247af1294ca6fcada5eca56be5e
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-08 16:16:50 +0700

    refactor webhook url 😅

--- routes/hook.php
+++ routes/hook.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('webhooks/{chatBot:callback_token}', WebhookController::class);
+Route::post('webhooks/messaging/{chatBot:callback_token}', WebhookController::class);
