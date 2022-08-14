0576e87ef78f7d0b0838892249b75105d96f60fc
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-14 14:16:29 +0700

    typo

--- app/Http/Middleware/InAppBrowsingNotAllow.php
+++ app/Http/Middleware/InAppBrowsingNotAllow.php
@@ -2,35 +2,26 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\InAppBrowsingAware;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Str;
-use Jenssegers\Agent\Agent;
 
 class InAppBrowsingNotAllow
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle(Request $request, Closure $next)
+    use InAppBrowsingAware;
+
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
     {
-        $agent = new Agent();
-
-        $agentName = trim($request->header('User-Agent').' '.($agent->isRobot() ? $agent->robot() : ''));
-
-        $inAppBrowsing =  preg_match("/Line\//i", $agentName) || preg_match('/facebook/i', $agentName);
-
-        if (! $inAppBrowsing) {
+        if (! $this->inAppBrowsing($request)) {
             return $next($request);
         }
 
         $token = Str::random(32);
         cache()->put(
-            Key:'in-app-browsing-redirect-token-'.$token,
+            key:'in-app-browsing-redirect-token-'.$token,
             value: $request->fullUrl(),
             ttl: now()->addMinutes(5)
         );
