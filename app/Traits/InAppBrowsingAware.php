1bbdc0caf739b606267ababa211ad268701e1e88
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-14 14:17:03 +0700

    add trait

---
+++ app/Traits/InAppBrowsingAware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
+
+trait InAppBrowsingAware
+{
+    protected function inAppBrowsing(Request $request): bool
+    {
+        $agent = new Agent();
+
+        $agentName = trim($request->header('User-Agent').' '.($agent->isRobot() ? $agent->robot() : ''));
+
+        return  preg_match("/Line\//i", $agentName) || preg_match('/facebook/i', $agentName);
+    }
+}
