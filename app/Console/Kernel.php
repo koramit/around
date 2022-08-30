08c6bbfd705f4122cd22225c8a6bbe1634c23650
Koramit Pichanaharee <koramit.p@gmail.com>
2022-08-30 19:03:16 +0700

    remove test exec

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,12 +14,12 @@ class Kernel extends ConsoleKernel
         $schedule->job(new NotifyDiscussionUpdates())->everyTenMinutes();
 
         /* Acute Hemodialysis */
-        $schedule->command('acute-hd:assign-an')->timezone('Asia/Bangkok')->at('13:00');
-        $schedule->exec('chown -R www-data:www-data storage')->timezone('Asia/Bangkok')->at('13:01');
-        $schedule->command('acute-hd:assign-an')->timezone('Asia/Bangkok')->at('18:55');
-        $schedule->exec('chown -R www-data:www-data storage')->timezone('Asia/Bangkok')->at('18:56');
+        $schedule->command('acute-hd:assign-an')->timezone('Asia/Bangkok')->at('11:00');
+        $schedule->exec('chown -R www-data:www-data storage')->timezone('Asia/Bangkok')->at('11:01');
         $schedule->job(new NotifyIncompleteOrderToAuthor())->timezone('Asia/Bangkok')->at('20:00');
         $schedule->job(new NotifyIncompleteOrderToAuthor())->timezone('Asia/Bangkok')->at('20:30');
+        $schedule->command('acute-hd:assign-an')->timezone('Asia/Bangkok')->at('23:00');
+        $schedule->exec('chown -R www-data:www-data storage')->timezone('Asia/Bangkok')->at('23:01');
         /** unsubscribe from inactive channel */
         // @TODO auto archived/expired acute HD case/order
     }
