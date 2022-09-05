393fd584c2efce50e7a4db239c0599023db2c0df
Koramit Pichanaharee <koramit.p@gmail.com>
2022-09-05 14:44:21 +0700

    refactor to route group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,10 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        'avatar' => [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
         'hook' => [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
