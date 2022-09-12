4a7764155094529fc5f124c5ee8b438d63a0d4c7
Koramit Pichanaharee <koramit.p@gmail.com>
2022-09-12 17:05:29 +0700

    add admission show action

---
+++ app/Actions/Resources/AdmissionShowAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Actions\Resources;
+
+use App\Managers\Resources\AdmissionManager;
+use App\Traits\AvatarLinkable;
+
+class AdmissionShowAction
+{
+    use AvatarLinkable;
+
+    public function __invoke(string $key): array
+    {
+        $link = $this->shouldLinkAvatar();
+        if ($link !== false) {
+            return $link;
+        }
+
+        $admission = (new AdmissionManager())->manage($key);
+
+        if (! $admission['found']) {
+            return $admission;
+        }
+
+        return [
+            'found' => true,
+            'hn' => $admission['admission']->patient->hn,
+            'name' => $admission['admission']->patient->full_name,
+            'gender' => $admission['admission']->patient->gender,
+            'age' => $admission['admission']->patient_age_at_encounter_text,
+            'ward_admit' => $admission['admission']->place_name,
+            'admitted_at' => $admission['admission']->encountered_at_for_humans,
+            'discharged_at' => $admission['admission']->dismissed_at_for_humans,
+        ];
+    }
+}
