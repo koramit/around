8d4d235079685388216d8fd4fb7dac7a5d3853e4
Koramit Pichanaharee <koramit.p@gmail.com>
2022-09-29 09:49:53 +0700

    tweak

--- app/Http/Middleware/PageTransition.php
+++ app/Http/Middleware/PageTransition.php
@@ -7,16 +7,9 @@ use Illuminate\Http\Request;
 
 class PageTransition
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle(Request $request, Closure $next)
     {
-        $current = app('request')->route()->getName();
+        $current = $request->route()->getName();
         session()->put('no-page-transition', session('no-page-transition-previous') === $current);
         session()->put('no-page-transition-previous', $current);
 
