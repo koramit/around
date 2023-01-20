91d3d307b157953976cbc91a6352251a6b23ee2c
Koramit Pichanaharee <koramit.p@gmail.com>
2023-01-20 16:14:18 +0700

    build

--- public/build/assets/MyDesk.1f345e31.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./app.2ee30176.js";const n={};function o(s,t){return c(),r("h1",null,"My Desk")}var _=e(n,[["render",o]]);export{_ as default};
--- public/build/assets/useConfirmForm.e5708921.js
+++
@@ -1,1 +0,0 @@
-import{r as m}from"./app.2ee30176.js";function c(){const r=m(null);return{confirmForm:r,openConfirmForm:o=>r.value.open(o),confirmed:(o,n)=>{n(o)}}}export{c as u};
