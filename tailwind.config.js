15eb6a0003e302317c3d5e35b82985a0c1b12570
koramit <koramit.p@gmail.com>
2024-02-04 15:39:22 +0700

    upgrade SW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+// noinspection JSUnusedGlobalSymbols
+
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
