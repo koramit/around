8b29d4dd003f5ac81088cde17543a709f7166e91
koramit <koramit.p@gmail.com>
2024-04-17 08:59:33 +0700

    built

---
+++ public/build/assets/NoBotAllow-DosGl5Kn.js
@@ -0,0 +1,1 @@
+import{b as e,c as t,d as s}from"./app-BbT5DcaR.js";const o={class:"w-full min-h-screen flex items-center justify-center"},n=s("h1",{class:"text-center font-semibold text-2xl md:text-4xl italic text-complement"}," Please open link in browser ",-1),c=[n],_={__name:"NoBotAllow",props:{layout:null},setup(l){return(a,r)=>(e(),t("main",o,c))}};export{_ as default};
