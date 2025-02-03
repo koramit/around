71eddb2acb5f2af18c6ec608a8413a54830f1c6b
koramit <koramit.p@gmail.com>
2025-02-03 08:45:44 +0700

    typo

--- app/Actions/Clinics/PostKT/CaseEditAction.php
+++ app/Actions/Clinics/PostKT/CaseEditAction.php
@@ -37,9 +37,6 @@ class CaseEditAction extends CaseBaseAction
                 $case->form[$field] = null;
             }
         }
-        if (! array_key_exists('donor_trauma', [...$case->form])) {
-            $case->form['donor_trauma'] = false;
-        }
         $case->save();
 
 
