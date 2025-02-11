e30e245fb1a8edfb974495c7454f3d53ed0d5043
koramit <koramit.p@gmail.com>
2025-02-11 22:26:34 +0700

    built

--- public/build/assets/useSelectOther-BtQ3ulzt.js
+++
@@ -1,1 +0,0 @@
-import{r as n,k as c}from"./app-BuHxGgPe.js";function o(){const s=n(null),e=c({placeholder:"",configs:null,input:""});return{selectOtherInput:s,selectOther:e,selectOtherClosed:(t,r=!1)=>{if(!t){e.input.setOther("");return}r?e.configs.push({value:t,label:t}):e.configs.push(t),e.input.setOther(t)}}}export{o as u};
