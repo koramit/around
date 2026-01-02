9eeddaa8f1e3a5734713b569f1796509ab05dca8
koramit <koramit.p@gmail.com>
2026-01-02 10:24:32 +0700

    add desogn page

---
+++ resources/js/Pages/Design.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <div class="p-12 bg-soft-theme-light text-gray-900">
+            soft-theme-light / primary
+            <span class="ml-4 text-green-500">primary-light</span>
+        </div>
+        <div class="p-12 bg-alt-theme-light text-gray-900">
+            alt-theme-light / primary-darker
+            <span class="ml-4 text-green-500">secondary</span>
+        </div>
+        <div class="p-12 bg-bitter-theme-light text-gray-900">
+            bitter-theme-light / accent
+            <span class="ml-4 text-green-500">primary</span>
+        </div>
+        <div class="p-12 bg-thick-theme-light text-gray-900">
+            thick-theme-light / complement
+            <span class="ml-4 text-green-500">complement</span>
+        </div>
+        <div class="p-12 bg-dark-theme-light text-gray-900">
+            dark-theme-light / complement-darker
+            <span class="ml-4 text-green-500">complement-dark</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
